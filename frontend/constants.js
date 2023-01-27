9718fb467f0c2998ab65eef41416e55de30ab476
Kc pele <fidekg123@gmail.com>
2023-01-27 18:38:49 +0100

    Update constants.js

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,7 +1,7 @@
 export const SUBGRAPH_URL =
   "https://api.thegraph.com/subgraphs/name/kcpele/celo-nft-marketplace";
 
-export const MARKETPLACE_ADDRESS = "0x21A11E6Cc6BB782D25c785624903BC88f14A6a7d"
+export const MARKETPLACE_ADDRESS = "0xE8aCD1eCeF8612682CA319A10107A6Bec8B22699"
 
 
 // cello 0x6510a0cd8d89428f62753ABB2d19d00678c70FA4
